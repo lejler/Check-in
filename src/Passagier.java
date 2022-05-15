919b00932eb1455a1dd943e69ef5e3341a60ae1c
llayla <layla.shokri@gmx.de>
2022-05-15 13:18:38 +0200

    added class Passagier

---
+++ src/Passagier.java
@@ -0,0 +1,39 @@
+public class Passagier {
+    
+    private String name;
+    private String vorname;
+    private String titel;
+    
+    public Passagier(String name, String vorname, String titel) {
+        this.name = name;
+        this.vorname = vorname;
+        this.titel = titel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    
+    
+}
